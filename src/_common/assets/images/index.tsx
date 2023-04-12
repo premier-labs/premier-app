815941591e803d03b2ed3798b36c046db1549ecf
Maxime Aubanel <maximeauba@gmail.com>
2023-04-12 10:59:42 +0200

    Fix Sensitive cases

--- src/_common/assets/images/index.tsx
+++ src/_common/assets/images/index.tsx
@@ -7,14 +7,14 @@ export { ReactComponent as PremierMagazineLogo } from "@common/assets/images/res
 export { ReactComponent as IconBook } from "@common/assets/images/resources/iconBook.svg";
 export { ReactComponent as IconDao } from "@common/assets/images/resources/iconDao.svg";
 export { ReactComponent as IconDiscord } from "@common/assets/images/resources/iconDiscord.svg";
-export { ReactComponent as IconGithub } from "@common/assets/images/resources/IconGithub.svg";
-export { ReactComponent as IconTwitter } from "@common/assets/images/resources/IconTwitter.svg";
-export { ReactComponent as IconInstagram } from "@common/assets/images/resources/IconInstagram.svg";
+export { ReactComponent as IconGithub } from "@common/assets/images/resources/iconGithub.svg";
+export { ReactComponent as IconTwitter } from "@common/assets/images/resources/iconTwitter.svg";
+export { ReactComponent as IconInstagram } from "@common/assets/images/resources/iconInstagram.svg";
 
 // Web3 Icons
-export { ReactComponent as IconEth } from "@common/assets/images/resources/IconEth.svg";
-export { ReactComponent as IconEtherscan } from "@common/assets/images/resources/IconEtherscan.svg";
-export { ReactComponent as IconOpenSea } from "@common/assets/images/resources/IconOpensea.svg";
+export { ReactComponent as IconEth } from "@common/assets/images/resources/iconEth.svg";
+export { ReactComponent as IconEtherscan } from "@common/assets/images/resources/iconEtherscan.svg";
+export { ReactComponent as IconOpenSea } from "@common/assets/images/resources/iconOpensea.svg";
 
 // Images
 import Deck_ from "@common/assets/images/resources/deck.png";
