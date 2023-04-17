69a0db20df7b02b60584aa3f360b6ad56615d244
Maxime Aubanel <maximeauba@gmail.com>
2023-04-17 16:53:13 +0200

    fix

--- src/_common/config/index.ts
+++ src/_common/config/index.ts
@@ -25,7 +25,7 @@ export const CONFIG = {
     server_url: import.meta.env.VITE__SERVER_URL!,
     websocket_server_url: import.meta.env.VITE__WEBSOCKET_SERVER_URL!,
   },
-  web3_provider_apiKey: import.meta.env.VITE__WEB3_PROVIDER_URL!,
+  web3_provider_apiKey: import.meta.env.VITE__WEB3_PROVIDER_API_KEY!,
   ipfs_provider_url: import.meta.env.VITE__IPFS_PROVIDER_URL!,
   blockExplorerUrl: import.meta.env.VITE__BLOCKEXPLORER!,
   openseaUrl: import.meta.env.VITE__OPENSEA_URL!,
