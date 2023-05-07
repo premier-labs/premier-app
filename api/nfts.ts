6fa579412aac90a0419f8c74ed40b9619cef2162
Maxime Aubanel <maximeauba@gmail.com>
2023-05-07 16:42:45 +0200

    Add opensea api for dev

--- api/nfts.ts
+++ api/nfts.ts
@@ -1,13 +1,18 @@
 import { Handler, HandlerContext, HandlerEvent } from "@netlify/functions";
 import { headers } from "./utils/http";
-import { OPENSEA } from "./utils/opensea";
+import { OPENSEA, getAssetsOwnedByAddress_Mock } from "./utils/opensea";
+import { CONFIG, isDevelopment } from "./utils/config";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   const { address } = event.queryStringParameters as any;
 
   return {
     statusCode: 200,
-    body: JSON.stringify(await OPENSEA.getAssetsOwnedByAddress(address)),
+    body: JSON.stringify(
+      isDevelopment
+        ? await getAssetsOwnedByAddress_Mock(address)
+        : await OPENSEA.getAssetsOwnedByAddress(address)
+    ),
     headers,
   };
 };
--- api/utils/opensea.ts
+++ api/utils/opensea.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 
 import axiosRetry from "axios-retry";
 import { CONFIG } from "./config";
+import { ListMockTokens } from "@premier-labs/contracts/dist/mock";
+import { provider } from "./provider";
+import { ERC721Enumerable__factory } from "@premier-labs/contracts/dist/typechain";
 
 axios.defaults.headers.common["X-API-KEY"] = CONFIG.opensea_api_key;
 
@@ -36,6 +39,40 @@ const getInCache = (contractAddress: string, tokenId: number) => {
   }
 
   return cachedValue;
+};
+
+export const getAssetsOwnedByAddress_Mock = async (address: string) => {
+  const NFTs: NFTs = [];
+  const dataToReturn: NFTsByCollection = [];
+
+  for (const listName in ListMockTokens) {
+    const list = ListMockTokens[listName];
+
+    const contract = ERC721Enumerable__factory.connect(list.contract, provider);
+    const nbAssets = (await contract.balanceOf(address)).toNumber();
+
+    for (let nb = 0; nb < nbAssets; nb++) {
+      const tokenId = (await contract.tokenOfOwnerByIndex(address, nb)).toNumber();
+
+      NFTs.push({
+        address: list.contract,
+        img: list.tokens[tokenId],
+        id: tokenId,
+        name: await contract.name(),
+        symbol: await contract.symbol(),
+      });
+    }
+  }
+
+  if (!NFTs.length) return [];
+
+  dataToReturn.push({
+    collectionName: NFTs[0].name,
+    collectionSymbol: NFTs[0].symbol,
+    assets: NFTs,
+  });
+
+  return dataToReturn;
 };
 
 export const getAsset = async (contractAddress: string, tokenId: number) => {
